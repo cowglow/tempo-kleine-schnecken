ff89be14091dc214d29abaa81c071f26854afff5
Philip Saa <cowglow@gmail.com>
2019-10-24 00:20:53 +0200

    Pages

---
+++ docs/precache-manifest.ad10c0f7b0299c0eb54074a89047347e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd986ef847213b8e87f5ad9028c5dcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f190dce6a63e43c21253",
+    "url": "/static/css/main.0d0f3792.chunk.css"
+  },
+  {
+    "revision": "37e00d4d15d5a21cc95c",
+    "url": "/static/js/2.7fa4c9dc.chunk.js"
+  },
+  {
+    "revision": "f190dce6a63e43c21253",
+    "url": "/static/js/main.53136b51.chunk.js"
+  },
+  {
+    "revision": "864b24d4d54457867127",
+    "url": "/static/js/runtime-main.f041f787.js"
+  }
+]);
