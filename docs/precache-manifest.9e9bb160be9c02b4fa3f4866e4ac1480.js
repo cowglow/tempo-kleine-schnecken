a6c2339990d555c4950cda57ec95d5029882fa8c
Philip Saa <cowglow@gmail.com>
2019-10-27 16:25:14 +0100

    Add docs for github pages

---
+++ docs/precache-manifest.9e9bb160be9c02b4fa3f4866e4ac1480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71509100f5e77cfad02b5e2f53836b3",
+    "url": "/tempo-kleine-schnecken/index.html"
+  },
+  {
+    "revision": "84a065f3d9e60d772313",
+    "url": "/tempo-kleine-schnecken/static/css/main.cc38cb6a.chunk.css"
+  },
+  {
+    "revision": "1518172901ae5644826f",
+    "url": "/tempo-kleine-schnecken/static/js/2.060c3828.chunk.js"
+  },
+  {
+    "revision": "84a065f3d9e60d772313",
+    "url": "/tempo-kleine-schnecken/static/js/main.f2943b5a.chunk.js"
+  },
+  {
+    "revision": "8403485965d0ed3d7a13",
+    "url": "/tempo-kleine-schnecken/static/js/runtime-main.4f09194e.js"
+  },
+  {
+    "revision": "90401f0828d7bf299627d4b4ed77ddb8",
+    "url": "/tempo-kleine-schnecken/static/media/snail-silhouette.90401f08.svg"
+  }
+]);
