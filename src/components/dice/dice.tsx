68dc4cde038719639385513fae7cc8f5e0be8024
Philip Saa <cowglow@gmail.com>
2020-12-29 14:18:22 +0100

    Added my own device motion handler.. it isn't perfect.

--- src/components/dice/dice.tsx
+++ src/components/dice/dice.tsx
@@ -5,10 +5,16 @@ import getRandomNumber from "../../lib/get-random-number/get-random-number";
 import { useGameState } from "../../context/game-state-context";
 
 const Dice = () => {
-  const { movePlayer } = useGameState();
+  const { movePlayer, positions, totalSteps } = useGameState();
+  const hasWinner = positions.some((position) => position > totalSteps - 1);
   const max = colors.length;
   const button = React.useRef(null);
   const [face, setFace] = React.useState("#000000");
+  const [sensorData, setSensorData] = React.useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
 
   const rollDice = () => {
     const randomIndex = getRandomNumber(max);
@@ -17,17 +23,45 @@ const Dice = () => {
     setFace(newValue);
   };
 
+  React.useEffect(() => {
+    const threshold: number = 15;
+
+    const handleMotionEvent = (event: DeviceMotionEvent) => {
+      requestAnimationFrame(() => {
+        const { x, y, z }: any = event.acceleration;
+        const xDiff = Math.abs(sensorData.x - x);
+        const yDiff = Math.abs(sensorData.y - y);
+        const zDiff = Math.abs(sensorData.z - z);
+        if (
+          (xDiff > threshold && yDiff > threshold) ||
+          (xDiff > threshold && zDiff > threshold)
+        ) {
+          if (!hasWinner) {
+            rollDice();
+            setSensorData({ x, y, z });
+          }
+        }
+      });
+    };
+
+    window.addEventListener("devicemotion", handleMotionEvent);
+
+    return () => window.removeEventListener("devicemotion", handleMotionEvent);
+  }, []);
+
   return (
-    <button
-      ref={button}
-      onClick={rollDice}
-      style={{
-        backgroundColor: face,
-      }}
-      className={style.root}
-    >
-      Click To Roll
-    </button>
+    <React.Fragment>
+      <button
+        ref={button}
+        onClick={rollDice}
+        style={{
+          backgroundColor: face,
+        }}
+        className={style.root}
+      >
+        Click To Roll
+      </button>
+    </React.Fragment>
   );
 };
 
